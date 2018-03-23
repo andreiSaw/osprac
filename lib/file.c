249ff2f41c9177b822eab7d6475aa66097f37790
macbook <isaevnextdoor@gmail.com>
2018-03-23 14:13:31 +0300

    lab10.1

--- lib/file.c
+++ lib/file.c
@@ -144,8 +144,10 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	int r;
 	fsipcbuf.write.req_fileid = fd->fd_file.id;
 	fsipcbuf.write.req_n = n < PGSIZE ? n: PGSIZE;
+	if ((r=fsipc(FSREQ_WRITE, NULL))<0)
+		return r;
 	memmove(fsipcbuf.write.req_buf, buf, fsipcbuf.write.req_n);
-	r = fsipc(FSREQ_WRITE, NULL);
+	//r = fsipc(FSREQ_WRITE, NULL);
 	return r;
 	//panic("devfile_write not implemented");
 }
