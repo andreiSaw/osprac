3705a43f8802d0ceb031161ba11a706a7e12ab1e
macbook <isaevnextdoor@gmail.com>
2018-03-27 17:05:49 +0300

    12.fix2

--- lib/vsyscall.c
+++ lib/vsyscall.c
@@ -5,7 +5,7 @@ static inline int32_t
 vsyscall(int num)
 {
 	// LAB 12: Your code here.
-	if (0 < num < NVSYSCALLS)
+	if ((num > 0)  && (num < NVSYSCALLS))
 		return vsys[num];
 	cprintf("0 < num<NVSYSCALLS!");
 	return 0;
