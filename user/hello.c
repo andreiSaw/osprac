1023eb8d6aecb6ba66f39f8915e50fe7484d380e
macbook <isaevnextdoor@gmail.com>
2018-03-29 00:39:44 +0300

    bug1

--- user/hello.c
+++ user/hello.c
@@ -1,12 +1,15 @@
 // hello, world
 #include <inc/lib.h>
+#define VA	((char *) 0xA0000000)
 
 void
 umain(int argc, char **argv)
 {
+	int r;
 	envid_t envid1 = thisenv->env_id;
 	cprintf("hello, world\n");
 	cprintf("i am environment %08x\n", envid1);
-	int r = sys_page_alloc(0, (void *)(UXSTACKTOP - PGSIZE), (PTE_U|PTE_P|PTE_W));
+	if ((r = sys_page_alloc(0, VA, (PTE_U|PTE_P|PTE_W))) < 0)
+		panic("sys_page_alloc: %i", r);
 	cprintf("page allocated %i\n", r);
 }
