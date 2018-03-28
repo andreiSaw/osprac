14fc170788847942c98a3e954e51878c7c7f96c1
macbook <isaevnextdoor@gmail.com>
2018-03-29 00:21:26 +0300

    test

--- user/hello.c
+++ user/hello.c
@@ -4,6 +4,9 @@
 void
 umain(int argc, char **argv)
 {
+	envid_t envid1 = thisenv->env_id;
 	cprintf("hello, world\n");
-	cprintf("i am environment %08x\n", thisenv->env_id);
+	cprintf("i am environment %08x\n", envid1);
+	int r = sys_page_alloc(envid1, 0xFF000000, (PTE_U|PTE_P|PTE_W));
+	cprintf("page allocated %i", r);
 }
