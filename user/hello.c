94cd2b4aebfa0fd44e8bbea39f5e3d5c8ac9f196
macbook <isaevnextdoor@gmail.com>
2018-03-29 16:30:46 +0300

    bug3

--- user/hello.c
+++ user/hello.c
@@ -5,11 +5,6 @@
 void
 umain(int argc, char **argv)
 {
-	int r;
-	envid_t envid1 = thisenv->env_id;
 	cprintf("hello, world\n");
-	cprintf("i am environment %08x\n", envid1);
-	if ((r = sys_page_alloc(envid1, VA, (PTE_U|PTE_P|PTE_W))) < 0)
-		panic("sys_page_alloc: %i\n", r);
-	cprintf("page allocated\n", r);
+	cprintf("i am environment %08x\n", thisenv->env_id);
 }
