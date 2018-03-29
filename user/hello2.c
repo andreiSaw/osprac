29f4b5a59689e37f850f4ec14c257c203797259d
macbook <isaevnextdoor@gmail.com>
2018-03-29 19:07:53 +0300

    bug9

--- user/hello2.c
+++ user/hello2.c
@@ -1,18 +1,6 @@
 // hello, world
 #include <inc/lib.h>
 
-void handler(struct UTrapframe *utf)
-{
-	int r;
-
-	void *addr = (void*)utf->utf_fault_va;
-
-	cprintf("fault %x\n", (uint32_t)addr);
-	if ((r = sys_page_alloc(0, ROUNDDOWN(addr, PGSIZE), (PTE_U|PTE_P|PTE_W))) < 0)
-		panic("allocating at %x in page fault handler: %i", (uint32_t)addr, r);
-	snprintf((char*) addr, 100, "this string was faulted in at %x", (uint32_t)addr);
-}
-
 void
 umain(int argc, char **argv)
 {
