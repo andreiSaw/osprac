bf2a63ab9956eb11f8e47fc3a30d61be220fb673
macbook <isaevnextdoor@gmail.com>
2018-03-29 19:13:54 +0300

    bug9

--- user/hello2.c
+++ user/hello2.c
@@ -5,9 +5,9 @@ void
 umain(int argc, char **argv)
 {
 	int r;
-	r = sys_page_alloc(sys_getenvid(), (void*)0xDeadBeef, (PTE_U|PTE_P|PTE_W));
+	r = sys_page_alloc(sys_getenvid(), (void*)0xdeadb000, (PTE_U|PTE_P|PTE_W));
 	cprintf("pages allocate\n\n");
-	cprintf("%s\n", (char*)0xDeadBeef);
+	cprintf("%s\n", (char*)0xdeadb000);
 	int i;
 	for (i = 0; i < USTACKTOP; i += PGSIZE) {
 		if ((uvpd[PDX(i)] & PTE_P) && // check if present
